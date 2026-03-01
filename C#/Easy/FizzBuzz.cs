c3ec4671fa1d8c55a317761c8f7a9faede0bef8e
Evan Davis <etdavis3652@gmail.com>
2026-03-01 15:18:11 -0500

    1 problem

---
+++ C#/Easy/FizzBuzz.cs
@@ -0,0 +1,27 @@
+//412. Fizz Buzz
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+*/
+
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        string[] answer = new string[n];
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                answer[i - 1] += "Fizz";
+            }
+            if (i % 5 == 0) {
+                answer[i - 1] += "Buzz";
+            }
+            if (answer[i - 1] == null) {
+                answer[i - 1] += i;
+            }
+        }
+        return answer;
+    }
+}
