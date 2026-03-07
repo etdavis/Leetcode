f4900356864a5c650eca5eb47f2a3f7f9fb963cb
Evan Davis <etdavis3652@gmail.com>
2026-03-06 23:57:17 -0500

    1 problem

---
+++ C#/Easy/FinalValueOfVariableAfterPerformingOperations.cs
@@ -0,0 +1,25 @@
+//2011. Final Value of Variable After Performing Operations
+/*
+There is a programming language with only four operations and one variable X:
+
+++X and X++ increments the value of the variable X by 1.
+--X and X-- decrements the value of the variable X by 1.
+Initially, the value of X is 0.
+
+Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+*/
+
+public class Solution {
+    public int FinalValueAfterOperations(string[] operations) {
+        int x = 0;
+        foreach (string operation in operations) {
+            if (operation.Contains('+')) {
+                x++;
+            }
+            else {
+                x--;
+            }
+        }
+        return x;
+    }
+}
